ab95da71a0dc31b1aa17cfbf33053920a8dccc9a
vaishali-sharma <iitmgwl1975@gmail.com>
2024-11-15 22:54:38 +0530

    implement api gateway microservice

---
+++ apiGateway/src/test/java/com/service/apiGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.apiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
